60d303aa4ce595bfec264269ceb20e2c65152e6d
Ivan Katliarchuk <ivan.katliarchuk@armakuni.com>
2019-01-08 21:03:59 +0000

    [#162982150] API Gateway is set.

--- bin/modules_pull.sh
+++ bin/modules_pull.sh
@@ -3,6 +3,8 @@
 git submodule add git@bitbucket.org-armakuni:armakuni/onse-transaction-service.git onse-transaction-service
 git submodule add git@bitbucket.org-armakuni:armakuni/onse-account-service.git onse-account-service
 git submodule add git@bitbucket.org-armakuni:armakuni/onse-customer-service.git onse-customer-service
+git submodule add git@bitbucket.org-armakuni:armakuni/onse-customer-service.git onse-customer-service
+git submodule add git@bitbucket.org-armakuni:armakuni/onse-cashier-service.git onse-cashier-service
 
 git submodule init
 # git submodule update
