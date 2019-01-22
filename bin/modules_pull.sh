304d29437939ddf1de3be69c9c8bfc3319e8fe08
Tom Oram <tom.oram@armkuni.com>
2019-01-22 14:07:55 +0000

    Update git submodel URLs

--- bin/modules_pull.sh
+++ bin/modules_pull.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-transaction-service.git onse-transaction-service
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-account-service.git onse-account-service
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-customer-service.git onse-customer-service
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-customer-service.git onse-customer-service
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-cashier-service.git onse-cashier-service
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-balance-service.git onse-balance-service
-git submodule add git@bitbucket.org-armakuni:armakuni/onse-balance-service.git
+git submodule add git@bitbucket.org:armakuni/onse-transaction-service.git onse-transaction-service
+git submodule add git@bitbucket.org:armakuni/onse-account-service.git onse-account-service
+git submodule add git@bitbucket.org:armakuni/onse-customer-service.git onse-customer-service
+git submodule add git@bitbucket.org:armakuni/onse-customer-service.git onse-customer-service
+git submodule add git@bitbucket.org:armakuni/onse-cashier-service.git onse-cashier-service
+git submodule add git@bitbucket.org:armakuni/onse-balance-service.git onse-balance-service
+git submodule add git@bitbucket.org:armakuni/onse-balance-service.git
 
 git submodule init
 git submodule update --remote --merge
